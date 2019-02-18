680b7acb3cf484bea94e9704d7341baff377b5dd
László Horváth <bedafoltyn@gmail.com>
2019-02-18 16:42:44 +0100

    db env config merged into one line, poll create route removed

--- app/router.js
+++ app/router.js
@@ -4,7 +4,6 @@ const pollController = require('./controllers/poll')
 const router = express.Router()
 
 router.get('/poll', pollController.list)
-router.post('/poll', pollController.create)
 router.get('/poll/:poll', pollController.get)
 router.post('/poll/:poll', pollController.vote)
 
--- index.js
+++ index.js
@@ -7,10 +7,7 @@ const router = require('./app/router')
 
 const app = express()
 
-mongoose.connect(
-  `mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@localhost:${process.env.DB_PORT}/${process.env.DB}`,
-  { useNewUrlParser: true }
-)
+mongoose.connect(`{process.env.DB}`, { useNewUrlParser: true })
 
 app.use(morgan('dev'))
 app.use(bodyParser.json())
